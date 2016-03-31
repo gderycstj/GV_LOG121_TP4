3df3dd03397db2fe797c1350bd97e1c84c2e6fe6
gderycstj <gabriodery@hotmail.com>
2016-03-31 11:42:42 -0400

    modif model

--- GestionImageTp4/src/GDVL/PerspectiveModel.java
+++ GestionImageTp4/src/GDVL/PerspectiveModel.java
@@ -1,6 +1,10 @@
 package GDVL;
 
 public class PerspectiveModel {
+	
+	private int niveauZoom;
+	private int degreTranslation;
+	
 	String Path;
 	
 	PerspectiveModel()
@@ -11,4 +15,36 @@ public class PerspectiveModel {
 	{
 		Path = pathImage;
 	}
+	
+	public void effectuerZoom()
+	{
+		niveauZoom+=20;
+		System.out.println("Niveau Zoom: " + niveauZoom);
+	}
+	
+	public void effectuerZoomContraire()
+	{
+		niveauZoom-=20;
+		System.out.println("Niveau Zoom: " + niveauZoom);
+	}
+	
+	public void effectuerTranslation()
+	{
+		degreTranslation+=20;
+		System.out.println("Niveau Translation: " + degreTranslation);
+		
+	}
+	
+	public void effectuerTranslationContraire()
+	{
+		degreTranslation-=20;
+		System.out.println("Niveau Translation: " + degreTranslation);
+	}
+	
+	public int getTranslation()
+	{
+		return degreTranslation;
+	}
+
 }
+
